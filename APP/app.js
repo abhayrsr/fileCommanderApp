4da87f187d8a214aeea0cb7497c317abe466195c
abhayrsr <abhayrajsinghrana123@gmail.com>
2025-01-03 00:25:37 +0530

    second commit

--- APP/app.js
+++ APP/app.js
@@ -1,15 +1,41 @@
-
-const { watch } = require("fs/promises")
+/*const { watch } = require('node:fs/promises');
+const ac = new AbortController();
+const { signal } = ac;
+setTimeout(() => ac.abort(), 10000);
 
 (async () => {
-	
-	try{
-		const watcher = watch('./')
-		for await (const event of watcher){
-			console.log(event)
-		}
-	} catch(error){
-		console.log(error)
-	}
+  try {
 
-})();
+    const watcher = await watch('./command.txt', {signal})
+
+    for await (const event of watcher)
+      console.log(event);
+    
+  } catch (err) {
+    if (err.name === 'AbortError')
+      return;
+    throw err;
+  }
+})();*/ 
+
+
+const fs = require('fs');
+
+const watcher = fs.watch('./command.txt', (eventType, filename) => {
+  if (filename) {
+    console.log({ eventType, filename });
+  }
+});
+
+// Run a blocking loop for 10 seconds
+const start = Date.now();
+const duration = 10000; // 10 seconds
+
+while (Date.now() - start < duration) {
+  // Keep the process alive
+}
+
+// Stop watching after 10 seconds
+watcher.close();
+console.log('Stopped watching');
+
