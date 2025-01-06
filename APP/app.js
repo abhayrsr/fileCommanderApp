f20f95ec6432479e7570dad216af105cfe095103
abhayrsr <abhayrajsinghrana123@gmail.com>
2025-01-07 01:08:25 +0530

    created emitter

--- APP/app.js
+++ APP/app.js
@@ -1,15 +1,38 @@
-const { watch } = require('node:fs/promises');
+const { watch, open, stat } = require('node:fs/promises');
 
 (async () => {
 
-  try {
-    const watcher = watch('./command.txt')
+    const commandFileHandler = await open('./command.txt', 'r');
+
+    commandFileHandler.on("change", async () => {
+    
+        const size = (await stat('./command.txt')).size;
+        //we want to read the content...
+        const buff = Buffer.alloc(size);
+        const offset = 0;
+        const length = buff.byteLength;
+        const position = 0;
+        const content = await commandFileHandler.read(
+          buff,
+          offset,
+          length,
+          position
+        );
+
+    });
+
+
+    const watcher = watch('./command.txt');
+
+    try{
 
     for await(const event of watcher){
-      console.log(event)
-      }
-    } catch(err){
-       throw err
-      }
+      commandFileHandler.emit("change");
+    }
+    } finally {
+      
+      await filehandle.close()
+    }
+
 })();
 
