a742a24f7457600442025dcb866573eee8f32a63
abhayrsr <abhayrajsinghrana123@gmail.com>
2025-01-08 03:05:23 +0530

    reading file

--- app.js
+++ app.js
@@ -3,31 +3,24 @@ const { watch, open, stat } = require('node:fs/promises');
 (async () => {
 
     const commandFileHandler = await open('./command.txt', 'r');
-    try{
-          commandFileHandler.on("change", async () => {
+
+    commandFileHandler.on("change", async () => {
+    
         const size = (await stat('./command.txt')).size;
         //we want to read the content...
         const buff = Buffer.alloc(size);
         const offset = 0;
         const length = buff.byteLength;
         const position = 0;
-        await commandFileHandler.read(
+        const content = await commandFileHandler.read(
           buff,
           offset,
           length,
           position
         );
-        console.log(buff.toString('utf-8'));
-    
+
     });
 
-
-
-
-    } catch(error){
-      console.log(error);
-
-    }
 
     const watcher = watch('./command.txt');
 
@@ -35,10 +28,9 @@ const { watch, open, stat } = require('node:fs/promises');
 
     for await(const event of watcher){
       commandFileHandler.emit("change");
-      }
-    } catch(error){
-
-      console.log(error);
+    }
+  } catch(error){
+      console.log('The error is :', error);
 
   } finally {
       
